20746d71218a9498b01cb81633a6ed2471b691fc
Utsanjan <59719700+utsanjan@users.noreply.github.com>
2021-05-08 17:36:26 +0530

    asset updates

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const FILE_PATH = "./data.json";
 
 const makeCommit = (n) => {
   if (n === 0) return simpleGit().push();
-  const x = random.int(0, 67);
-  const y = random.int(0, 9);
+  const x = random.int(0, 50);
+  const y = random.int(0, 7);
   const DATE = moment()
-    .subtract(1, "y")
+    .subtract(30, "w")
     .add(1, "d")
     .add(x, "w")
     .add(y, "d")
